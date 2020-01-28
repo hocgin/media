aa8b9d7279e68f67d554183f90c4e78efb78e7fb
hocgin <hocgin@gmail.com>
2020-01-28 17:17:35 +0800

    # INDEX

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,9 @@ import styles from './index.less';
 export default function() {
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
+      <a href="/photo">图片s</a><br/>
+      <a href="/video">视频s</a><br/>
+      <a href="/music">音乐s</a><br/>
     </div>
   );
 }
--- src/pages/photo/index.js
+++ src/pages/photo/index.js
@@ -3,6 +3,7 @@ import { connect } from 'dva';
 import styles from './index.less';
 import ImageCard from '@/components/ImageCard';
 import Img from 'react-image';
+import { Col, Row } from 'antd';
 
 @connect((...rest) => {
   console.log('arg1', rest);
@@ -26,21 +27,34 @@ class index extends React.Component {
           </div>
         </div>
         {/* HEAD */}
-        {/*  BOX  */}
+        {/*BOXv2*/}
         <div className={styles.photos}>
-          <div className={styles.photoColumn}>
-            {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}
-          </div>
-          <div className={styles.photoColumn}>
-            {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}
-          </div>
-          <div className={styles.photoColumn}>
-            {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}
-          </div>
-          <div className={styles.photoColumn}>
-            {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}
-          </div>
+          <Row gutter={[{ xs: 5, sm: 16 }, 0]}>
+            {[1, 2, 3, 4].map((item, index) => (
+              <Col xs={24} sm={6}>
+                {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}
+              </Col>
+            ))}
+          </Row>
         </div>
+        {/*BOXv2*/}
+
+
+        {/*  BOX  */}
+        {/*<div className={styles.photos}>*/}
+        {/*  <div className={styles.photoColumn}>*/}
+        {/*    {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}*/}
+        {/*  </div>*/}
+        {/*  <div className={styles.photoColumn}>*/}
+        {/*    {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}*/}
+        {/*  </div>*/}
+        {/*  <div className={styles.photoColumn}>*/}
+        {/*    {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}*/}
+        {/*  </div>*/}
+        {/*  <div className={styles.photoColumn}>*/}
+        {/*    {([1, 2, 3]).map(() => <ImageCard wrapperClassName={styles.photoImage} src={that.getOneImage()}/>)}*/}
+        {/*  </div>*/}
+        {/*</div>*/}
         {/*  BOX  */}
       </div>
     );
