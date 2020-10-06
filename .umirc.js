096f004cce8311858ca208647613a320f23022cb
hocgin <hocgin@gmail.com>
2020-10-06 09:06:37 +0800

    Update

--- .umirc.js
+++ .umirc.js
@@ -1,6 +1,5 @@
 // ref: https://umijs.org/config/
 export default {
-  treeShaking: true,
   exportStatic: {
     dynamicRoot: true,
   },
@@ -15,25 +14,5 @@ export default {
       {path: '/photo', component: '../pages/photo/index'}
     ]
   }],
-  plugins: [
-    // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: true,
-      dva: true,
-      dynamicImport: false,
-      title: 'media',
-      dll: true,
-
-      routes: {
-        exclude: [
-          /models\//,
-          /services\//,
-          /model\.(t|j)sx?$/,
-          /service\.(t|j)sx?$/,
-          /components\//,
-        ],
-      },
-    }],
-  ],
   copy: ['CNAME'],
 }
